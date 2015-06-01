e3084729be956244e579018d46a67db6ebece47e
simingl <bryan.liusiming@gmail.com>
2015-06-01 16:56:09 -0700

    init

---
+++ Assets/Scripts/ChangePOV.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangePOV : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject camFirst;
+	public GameObject camThird;
+	public GameObject camMain;
+
+	private int currentCamera;
+
+	// Use this for initialization
+	void Start () {
+		camThird.gameObject.SetActive (false);
+		camFirst.gameObject.SetActive (false);
+		camMain.gameObject.SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F1)) {
+			camFirst.gameObject.SetActive (true);
+			camMain.gameObject.SetActive (false);
+			camThird.gameObject.SetActive (false);
+		}else if(Input.GetKeyDown(KeyCode.F2)){
+			camFirst.gameObject.SetActive (false);
+			camMain.gameObject.SetActive (true);
+			camThird.gameObject.SetActive (false);
+		}else if(Input.GetKeyDown(KeyCode.F3)){
+			camFirst.gameObject.SetActive (false);
+			camMain.gameObject.SetActive (false);
+			camThird.gameObject.SetActive (true);
+		}
+	}
+}
---
+++ Assets/Scripts/DroneController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DroneController : MonoBehaviour {
+
+	public float turnSmoothing = 15f;
+	public float speedDampTime = 0.1f;
+	public float speedUpTime   = 0.1f;
+	public float speedUp = 3f;
+
+	public Rigidbody player;
+	
+	void Awake(){
+		player = GetComponent<Rigidbody> ();
+	}
+	
+	void FixedUpdate(){
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+		float j = Input.GetAxis ("Jump");
+
+		MovementManagement (h,j,v);
+	}
+	
+	void MovementManagement(float horizontal, float jump, float vertical){
+		if (horizontal != 0f || vertical != 0f || jump != 0f) {
+			this.Rotating(horizontal,vertical);
+
+			this.Moving(horizontal,jump, vertical);
+		}
+	}
+	
+	void Rotating(float horizontal, float vertical){
+		Vector3 targetDirection = new Vector3 (horizontal, 0f, vertical);
+		Quaternion targetRotation = Quaternion.LookRotation (targetDirection, Vector3.up);
+		Quaternion newRotation = Quaternion.Lerp (player.rotation, targetRotation, turnSmoothing*Time.deltaTime);
+		player.MoveRotation (newRotation);
+	}
+
+	void Moving(float horizontal, float jump, float vertical){
+		Vector3 oldPosition = player.position;
+		Vector3 targetPosition = new Vector3(oldPosition.x+horizontal, oldPosition.y + jump, oldPosition.z+vertical);
+		Vector3 newPosition = Vector3.Lerp (oldPosition, targetPosition, speedDampTime * Time.deltaTime);
+		player.MovePosition (newPosition);
+	}
+}
